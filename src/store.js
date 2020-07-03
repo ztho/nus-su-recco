66df8a324b5fe09aa5325b983e795895e582485d
Zheng Ting <zhengtingho97@gmail.com>
2020-07-03 15:20:15 +0800

    Change redux dev tools

--- src/store.js
+++ src/store.js
@@ -31,7 +31,7 @@ const persistedState = loadState();
 
 const store = createStore(rootReducer, persistedState, compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
     )
 ); 
 
