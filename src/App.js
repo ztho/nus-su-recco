62855a1efab64297a87d0190e58d7b33c19cf795
Zheng Ting <zhengtingho97@gmail.com>
2020-07-03 13:11:52 +0800

    Removed CSS Dependency

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import React, { Component } from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css"; 
 import AppNavBar from "./components/AppNavbar";
 import AddKeyData from "./components/AddKeyData";
